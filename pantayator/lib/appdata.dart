dc464a3ce5a2607d3c123442d4166467a9978acf
VDani3 <villacarmona.dani@gmail.com>
2023-11-09 17:58:15 +0100

    Spect 5 i 6

--- pantayator/lib/appdata.dart
+++ pantayator/lib/appdata.dart
@@ -1,6 +1,53 @@
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:web_socket_channel/io.dart';
 
 class AppData with ChangeNotifier {
+  String ip = "";
+  String text = "";
+  bool connected = false;
 
+  //WebSocket
+  IOWebSocketChannel? _server;
+
+  void connectServer() async {
+    _server = IOWebSocketChannel.connect("ws://$ip:8888");
+
+    //Mandar missatge de la versió de la app que es
+    _server?.sink.add('{"type":"connection", "version": "desktop"}');
+
+    //Quan rep un missatge
+    _server!.stream.listen((message) {
+      final data = jsonDecode(message);
+
+      /*
+       * Aqui que fer amb els missatges que rep
+       */
+
+      connected = true;
+      notifyListeners();
+    }, onError: (error) {
+      //Si dona un error es reinicia tot
+      connected = false;
+      notifyListeners();
+    });
+  }
+
+  //Si es vol desconectar del server
+  disconnectedFromServer() async {
+    connected = false;
+    notifyListeners();
+
+    _server!.sink.close();
+  }
+
+  //Mandar missatge per a que ho print
+  void showTextMessage() {
+    final msn = {
+      'type': 'show',
+      'value': text
+    };
+    _server?.sink.add(jsonEncode(msn));
+  }
 }
--- pantayator/lib/pages/lobby.dart
+++ pantayator/lib/pages/lobby.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pantayator/appdata.dart';
+import 'package:provider/provider.dart';
 
 class Lobby extends StatefulWidget {
   const Lobby({super.key});
@@ -11,6 +13,13 @@ class Lobby extends StatefulWidget {
 class _LobbyState extends State<Lobby> {
   @override
   Widget build(BuildContext context) {
+    //Per poder accedir-hi a la informació de la AppData
+    AppData appData = Provider.of<AppData>(context);
+
+    //Per poder controlar els TextFields
+    final _ipTextController = TextEditingController();
+    final _textController = TextEditingController();
+
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
           middle: Text(
@@ -23,18 +32,59 @@ class _LobbyState extends State<Lobby> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 50,),
-                _createTextField("Missatge", "Hola Mundo"),
-                SizedBox(height: 32,),
-                _createTextField("Conexió IP", "192.168.0.2x"),
-                SizedBox(height: 56,),
-                CupertinoButton(
-                  child: Text('Connectar'), 
-                  color: CupertinoColors.activeBlue,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  onPressed:() {
-                  
-                  },
+                SizedBox(
+                  height: 50,
+                ),
+                _createTextField("Missatge", "Hola Mundo", _textController),
+                SizedBox(
+                  height: 32,
+                ),
+                _createTextField(
+                    "Conexió IP", "192.168.0.2x", _ipTextController),
+                SizedBox(
+                  height: 76,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //Botó per connectar
+                    Container(
+                      width: 225,
+                      child: CupertinoButton(
+                        padding: EdgeInsets.all(5),
+                        child: Text('Connectar'),
+                        color: CupertinoColors.activeBlue,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        onPressed: () {
+                          setState(() {
+                            appData.ip = _ipTextController.text;
+                            appData.connectServer();
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    //Botó de enviar missatge, si esta connectat estará verd, si no vermell
+                    Container(
+                      width: 225,
+                      child: CupertinoButton(
+                          padding: EdgeInsets.all(5),
+                          child: Text('Enviar Missatge'),
+                          color: appData.connected
+                              ? CupertinoColors.activeGreen
+                              : CupertinoColors.destructiveRed,
+                          onPressed: () {
+                            if (appData.connected == true) {
+                              setState(() {
+                                appData.text = _textController.text;
+                                appData.showTextMessage();
+                              });
+                            }
+                          }),
+                    )
+                  ],
                 )
               ],
             ),
@@ -42,7 +92,9 @@ class _LobbyState extends State<Lobby> {
         ));
   }
 
-  Column _createTextField(String titol, String defaultText) {
+  //Amb aquesta funció creem un TextField i el seu titol
+  Column _createTextField(
+      String titol, String defaultText, TextEditingController controller) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -56,9 +108,13 @@ class _LobbyState extends State<Lobby> {
         Container(
           width: 225,
           child: CupertinoTextField(
+            decoration: BoxDecoration(
+                border: Border.all(color: Color.fromARGB(255, 105, 105, 105)),
+                borderRadius: BorderRadius.all(Radius.circular(8))),
             padding: EdgeInsets.all(10),
             textAlign: TextAlign.left,
             placeholder: defaultText,
+            controller: controller,
           ),
         ),
       ],
